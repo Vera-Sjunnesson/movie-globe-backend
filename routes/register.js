a274224e05fab6bc4cef7d24646d21ba381db7ea
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-06-01 16:33:01 +0200

    lifted registercontroller to own file

---
+++ routes/register.js
@@ -0,0 +1,57 @@
+import bcrypt from 'bcrypt';
+import { User } from '../models/userModel';
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+    // At least one digit
+    // At least one lowercase letter
+    // At least one uppercase letter
+    // At least 6 characters long
+  
+    //Checks if valid password
+     if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        success: false,
+        response: {
+          message: "Password needs to be at least 6 characters, and include one number, one uppercase letter and one lowercase letter."
+        }
+      });
+    };
+    // checks username available
+    try {
+      const usernameTaken = await User.findOne({ username: username});
+      if (usernameTaken) {
+        return res.status(400).json({
+          success: false,
+          response: {
+            message: "Username is already taken"
+          }
+        });
+      };
+  
+      // encryption
+      const salt = bcrypt.genSaltSync();
+  
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save();
+  
+       res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          id: newUser._id,
+          accessToken: newUser.accessToken,
+          message: "User successfully created"
+        }
+      });
+  
+    } catch (err) {
+       res.status(400).json({
+        success: false,
+        response: err
+      });
+    };
+  });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import mongoose from 'mongoose';
-import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 import movieJson from './data/movies.json';
 import { Movie } from "./models/movieModel";
 import { User } from "./models/userModel";
 import dotenv from 'dotenv';
+import { registerController } from "./controllers/registerController";
 dotenv.config();
 
 
@@ -126,60 +126,7 @@ app.post("/movies", async (req, res) => {
 });
 
 // Register user
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-  // At least one digit
-  // At least one lowercase letter
-  // At least one uppercase letter
-  // At least 6 characters long
-
-  //Checks if valid password
-   if (!passwordRegex.test(password)) {
-    return res.status(400).json({
-      success: false,
-      response: {
-        message: "Password needs to be at least 6 characters, and include one number, one uppercase letter and one lowercase letter."
-      }
-    });
-  };
-  // checks username available
-  try {
-    const usernameTaken = await User.findOne({ username: username});
-    if (usernameTaken) {
-      return res.status(400).json({
-        success: false,
-        response: {
-          message: "Username is already taken"
-        }
-      });
-    };
-
-    // encryption
-    const salt = bcrypt.genSaltSync();
-
-    const newUser = await new User({
-      username: username,
-      password: bcrypt.hashSync(password, salt)
-    }).save();
-
-     res.status(201).json({
-      success: true,
-      response: {
-        username: newUser.username,
-        id: newUser._id,
-        accessToken: newUser.accessToken,
-        message: "User successfully created"
-      }
-    });
-
-  } catch (err) {
-     res.status(400).json({
-      success: false,
-      response: error
-    });
-  };
-});
+app.post("/register", registerController);
 
 /* LOGIN USER */
 app.post("/login", async (req, res) => {
