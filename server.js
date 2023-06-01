a6c6df498b7a830852b98795a444447edbeabc99
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-06-01 12:10:30 +0200

    added endpoints for register and log in

--- server.js
+++ server.js
@@ -20,10 +20,33 @@ const app = express();
 const listEndPoints = require('express-list-endpoints');
 
 
-// Add middlewares to enable cors and json body parsing
+/* Middlewares */
 app.use(cors());
 app.use(express.json());
+// Authenticate the user
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization');
+  try {
+    const user = await User.findOne({accessToken});
+    if (user) {
+      next();
+    } else {
+      res.status(400).json({
+        success: false,
+        response: {
+          message: "User not logged in",
+        }
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error
+    });
+  }
+};
 
+/* Schemas */
 const { Schema } = mongoose;
 
 const MovieSchema = new Schema({
@@ -32,24 +55,10 @@ const MovieSchema = new Schema({
   key_scene: String,
   location_image: String,
   coordinates: [Number] // [longitude, latitude]
-})
-
+});
 const Movie = mongoose.model("Movie", MovieSchema);
 
-// Resetting the db  // Runs with RESET_DB=true (from .env)
-if (process.env.RESET_DB) {
-  const resetDatabase = async () => {
-    await Movie.deleteMany();
-    movieJson.forEach(movie => {
-      const newMovie = new Movie(movie);
-      newMovie.save();
-    })
-  };
-  resetDatabase();
-};
-
-
-/* const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -67,10 +76,22 @@ if (process.env.RESET_DB) {
     default: () => crypto.randomBytes(128).toString('hex')
   }
 });
+const User = mongoose.model("User", UserSchema);
 
-const User = mongoose.model("User", UserSchema); */
 
-// Start defining your routes here
+/* Resetting the db  // Runs with RESET_DB=true (from .env) */
+if (process.env.RESET_DB) {
+  const resetDatabase = async () => {
+    await Movie.deleteMany();
+    movieJson.forEach(movie => {
+      const newMovie = new Movie(movie);
+      newMovie.save();
+    })
+  };
+  resetDatabase();
+};
+
+/********* GET REQUESTS **********/
 app.get("/", (req, res) => {
   const message = "Welcome to the MovieGlobe API"
   const endpoints = (listEndPoints(app))
@@ -112,6 +133,8 @@ app.get('/movies', async (req, res) => {
   }
 });
 
+
+/******* POST REQUESTS ***********/
 //POST movie to database
 app.post("/movies", async (req, res) => {
   const { title, key_scene, location_image } = req.body;
@@ -132,11 +155,121 @@ app.post("/movies", async (req, res) => {
   }
 });
 
+// Register user
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+  // At least one digit
+  // At least one lowercase letter
+  // At least one uppercase letter
+  // At least 6 characters long
+
+  //Checks if valid password
+   if (!passwordRegex.test(password)) {
+    return res.status(400).json({
+      success: false,
+      response: {
+        message: "Password needs to be at least 6 characters, and include one number, one uppercase letter and one lowercase letter."
+      }
+    });
+  };
+  // checks username available
+  try {
+    const usernameTaken = await User.findOne({ username: username});
+    if (usernameTaken) {
+      return res.status(400).json({
+        success: false,
+        response: {
+          message: "Username is already taken"
+        }
+      });
+    };
+
+    // encryption
+    const salt = bcrypt.genSaltSync();
+
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+
+     res.status(201).json({
+      success: true,
+      response: {
+        username: newUser.username,
+        id: newUser._id,
+        accessToken: newUser.accessToken,
+        message: "User successfully created"
+      }
+    });
+
+  } catch (err) {
+     res.status(400).json({
+      success: false,
+      response: error
+    });
+  };
+});
+
+/* LOGIN USER */
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({username});
+    // checks password
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken,
+          message: "User logged in"
+        }
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        response: {
+          message: "Credentials do not match",
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error
+    })
+  }
+});
+
+
+/* Restricted endpoints by authenticateUser() */
+app.get("/vipmovies", authenticateUser);
+app.get("/vipmovies", async (req, res) => {
+  try { 
+    res.status(200).json({
+      success: true,
+      response: {
+        movies: 'Returns full access movielist'
+      }
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: {
+        message: error
+      }
+    }); 
+  };
+});
+
 //ENDPOINTS accessed WITHOUT accessToken:
 
 //User model:
-//("/register")
-//("/login")
+//("/register") - DONE!
+//("/login") - DONE!
 
 //Movies model:
 //app.get("/movies" - version for landing page with fewer movies - and a few query values/filters, such as genre
